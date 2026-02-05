dfca990e0d941199bc8145093f196ebdc19e5577
ka12438 <ka12438@auca.kg>
2026-02-05 11:57:13 +0600

    Reformat

--- lab14/lab14.py
+++ lab14/lab14.py
@@ -73,38 +73,7 @@ countdown_timer (10)
 
 #Lab Exercise 4: Creating a Simple Month Calendar
 from datetime import datetime, timedelta
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
-22
-23
-24
-25
-26
-27
-28
-29
-30
-31
-32
-33
-34
-35
-36
-37
-38
+
 def simple_month_calendar (year, month) :
 
     current_date = datetime (year, month, 1)
